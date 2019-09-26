66bb8e58efd21404e710a63d358e394b64724c3f
herkommer <m@herkommer.co.uk>
2019-09-26 10:15:51 +0200

    Sample ref vs value types

--- Objektorientering/ReferenceTypeDemo/Program.cs
+++ Objektorientering/ReferenceTypeDemo/Program.cs
@@ -10,6 +10,25 @@ namespace ReferenceTypeDemo
     {
         static void Main(string[] args)
         {
+            int i = 5;
+            int j = 10;
+
+            i = j;
+            Console.WriteLine("i={0}, j={1}",i,j);
+
+            Customer c = new Customer();
+            c.Name = "Kalle";
+
+            Customer d;
+            d = c;
+            d.Name = "Arne";
+
+            Console.WriteLine("Kunden c's namn: {0}, d's namn: {1}",c.Name,d.Name );
         }
+    }
+
+    class Customer
+    {
+        public string Name { get; set; }
     }
 }
